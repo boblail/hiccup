25e30e47bf92edca7aa69bff94b2415ffb7ea2b1
Bob Lail <bob.lail@cph.org>
2013-09-17 12:24:32 -0500

    [refactor] Extracted 'Scorer' from 'Guesser' (10m)

--- lib/hiccup/inferable/guesser.rb
+++ lib/hiccup/inferable/guesser.rb
@@ -1,3 +1,5 @@
+require 'hiccup/inferable/scorer'
+
 module Hiccup
   module Inferable
     class Guesser
@@ -5,10 +7,11 @@ module Hiccup
       def initialize(klass, options={})
         @klass = klass
         @verbose = options.fetch(:verbose, false)
+        @scorer = options.fetch(:scorer, Scorer.new(options))
         start!
       end
       
-      attr_reader :confidence, :schedule, :dates
+      attr_reader :confidence, :schedule, :dates, :scorer
       
       def start!
         @dates = []
@@ -38,7 +41,7 @@ module Hiccup
       
       def best_schedule_for(dates)
         guesses = generate_guesses(dates)
-        pick_best_guess(guesses, dates)
+        scorer.pick_best_guess(guesses, dates)
       end
       
       def generate_guesses(dates)
@@ -156,58 +159,6 @@ module Hiccup
           at_popularities = popularities.take(i + 1)
           yield values_by_popularity.values_at(*at_popularities).flatten(1)
         end
-      end
-      
-      
-      
-      def pick_best_guess(guesses, dates)
-        scored_guesses = guesses \
-          .map { |guess| [guess, score_guess(guess, dates)] } \
-          .sort_by { |(guess, score)| -score.to_f }
-        
-        if @verbose
-          puts "\nGUESSES FOR #{dates}:"
-          scored_guesses.each do |(guess, score)|
-            puts "  (%.3f p/%.3f b/%.3f c/%.3f) #{guess.humanize}" % [
-              score.to_f,
-              score.prediction_rate,
-              score.brick_penalty,
-              score.complexity_penalty]
-          end
-          puts ""
-        end
-        
-        scored_guesses.first
-      end
-      
-      def score_guess(guess, input_dates)
-        predicted_dates = guess.occurrences_between(guess.start_date, guess.end_date)
-        
-        # prediction_rate is the percent of input dates predicted
-        predictions = (predicted_dates & input_dates).length
-        prediction_rate = Float(predictions) / Float(input_dates.length)
-        
-        # bricks are dates predicted by this guess but not in the input
-        bricks = (predicted_dates - input_dates).length
-        
-        # brick_rate is the percent of bricks to predictions
-        # A brick_rate >= 1 means that this guess bricks more than it predicts
-        brick_rate = Float(bricks) / Float(input_dates.length)
-        
-        # complexity measures how many rules are necesary
-        # to describe the pattern
-        complexity = complexity_of(guess)
-        
-        # complexity_rate is the number of rules per inputs
-        complexity_rate = Float(complexity) / Float(input_dates.length)
-        
-        Score.new(prediction_rate, brick_rate, complexity_rate)
-      end
-      
-      def complexity_of(schedule)
-        return schedule.weekly_pattern.length if schedule.weekly?
-        return schedule.monthly_pattern.length if schedule.monthly?
-        1
       end
       
     end
