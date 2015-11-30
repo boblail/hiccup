9f0124676fab2e3766e6e953eccbbfc3821975c6
Ben Govero <ben.govero@cph.org>
2015-11-30 14:39:17 -0600

    Removed superfluous carriage returns

--- test/validatable_test.rb
+++ test/validatable_test.rb
@@ -3,28 +3,28 @@ require "test_helper"
 
 class ValidatableTest < ActiveSupport::TestCase
   include Hiccup
-  
-  
-  
+
+
+
   def test_invalid_recurrence
     # Test nil weekly recurrence
     r = Schedule.new(:kind => :weekly)
     assert !r.valid?,                                   "Recurrence should be invalid: weekly_pattern is empty"
     assert r.errors[:weekly_pattern].any?,              "weekly_pattern should be invalid if empty and kind is 'weekly'"
-    
+
     # Test nil monthly recurrence
     r = Schedule.new(:kind => :monthly)
     assert !r.valid?,                                   "Recurrence should be invalid: monthly_pattern is empty"
     assert r.errors[:monthly_pattern].any?,             "monthly_pattern should be invalid if empty and kind is 'monthly'"
-    
+
     # Test invalid monthly recurrence
     r = Schedule.new(:kind => :monthly, :monthly_pattern => [[2, "holiday"]])
     assert !r.valid?,                                   "Recurrence should be invalid: monthly_pattern is invalid"
     assert r.errors[:monthly_pattern].any?,             "monthly_pattern should be invalid: 'holiday' is not a valid MonthlyOccurrenceType"
   end
-  
-  
-  
+
+
+
   def test_temporal_paradoxes
     recurrence = Schedule.new({
       :kind => :annually,
@@ -41,33 +41,33 @@ class ValidatableTest < ActiveSupport::TestCase
     })
     assert !recurrence.valid?,                          "Recurrence should be invalid: its recurrence ends before it starts"
   end
-  
-  
-  
+
+
+
   def test_valid_weekly_recurrence
     recurrence = Schedule.new(:kind => :weekly, :weekly_pattern => %w{Tuesday})
     assert_valid(recurrence)
   end
-  
-  
-  
+
+
+
   def test_valid_monthly_recurrence
     recurrence = Schedule.new(:kind => :monthly, :monthly_pattern => [2])
     assert_valid(recurrence)
-    
+
     recurrence = Schedule.new(:kind => :monthly, :monthly_pattern => [[2, "Thursday"]])
     assert_valid(recurrence)
   end
-  
-  
-  
+
+
+
   def test_valid_annual_recurrence
     recurrence = Schedule.new(:kind => :annually)
     assert_valid(recurrence)
   end
-  
-  
-  
+
+
+
   context "skip" do
     should "be positive" do
       schedule = Schedule.new
@@ -75,14 +75,14 @@ class ValidatableTest < ActiveSupport::TestCase
       schedule.valid?
       assert schedule.errors[:skip].any?, "Expected skip to be invalid"
     end
-    
+
     should "be present" do
       schedule = Schedule.new
       schedule.skip = nil
       schedule.valid?
       assert schedule.errors[:skip].any?, "Expected skip to be invalid"
     end
-    
+
     should "be nonzero" do
       schedule = Schedule.new
       schedule.skip = 0
@@ -90,9 +90,9 @@ class ValidatableTest < ActiveSupport::TestCase
       assert schedule.errors[:skip].any?, "Expected skip to be invalid"
     end
   end
-  
-  
-  
+
+
+
   context "When an end_date is supplied but not a start_date, it" do
     should "record an error for the start_date but not for end_date" do
       schedule = Schedule.new(kind: :weekly, ends: true, end_date: Date.today)
@@ -102,17 +102,17 @@ class ValidatableTest < ActiveSupport::TestCase
       refute schedule.errors[:end_date].any?
     end
   end
-  
-  
-  
+
+
+
 private
-  
-  
-  
+
+
+
   def assert_valid(record)
     assert record.valid?, record.errors.inspect
   end
-  
-  
-  
+
+
+
 end
